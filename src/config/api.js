62ecfeb96209f6933f0ec657dd641fef692bc95a
Rawakinodes <59562911+rawakinode@users.noreply.github.com>
2025-12-20 20:48:45 +0800

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-// export const API_URL="https://swifter-api.vercel.app"
-export const API_URL="http://localhost:3300"
+export const API_URL="https://swifter-v2-api.vercel.app/"
+// export const API_URL="http://localhost:3300"
